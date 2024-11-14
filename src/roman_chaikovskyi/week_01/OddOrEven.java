dfee002b0bde2c7c2af19edf84e318b0c9945303
Roman <romanosik01@gmail.com>
2024-11-14 16:33:45 -0500

    Finish week01. Done with homework.

--- src/roman_chaikovskyi/week_01/OddOrEven.java
+++ src/roman_chaikovskyi/week_01/OddOrEven.java
@@ -1,8 +1,24 @@
 package roman_chaikovskyi.week_01;
 
+import static java.lang.System.*;
+
 public class OddOrEven {
 
+    public static void main(String[] args) {
+        out.println(oddOrEven(5));
+        out.println(oddOrEven(6));
+        out.println(oddOrEven(55));
+        out.println(oddOrEven(22));
+    }
 
+    public static String oddOrEven(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+
+    }
 
 }
 
