33de28de7e691c756055a6c8441c34dd58e89b31
Ersin <michael.knight@cydeo.com>
2024-11-14 20:09:16 +0300

    Added Odd_Or_Even Class

---
+++ src/michael/week_1/Odd_Or_Even.java
@@ -0,0 +1,23 @@
+package michael.week_1;
+
+public class Odd_Or_Even {
+
+ /*
+    Write  a method which can identifies given number is even or odd
+EX:
+identify(5) ->  "Odd"
+identify(6) ->  "Even"
+
+     */
+
+
+    public static void  oddOrEven(int n) {
+
+
+
+    }
+
+    public static void main(String[] args) {
+        oddOrEven(10);
+    }
+}
