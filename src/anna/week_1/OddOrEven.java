a8e75152e49e8d2aff198ca02b00483b45b069e5
annaplaton <145874345+annaplaton@users.noreply.github.com>
2024-11-14 12:18:11 -0500

    Added Odd Or Even Class updated

--- src/anna/week_1/OddOrEven.java
+++ src/anna/week_1/OddOrEven.java
@@ -1,4 +1,23 @@
 package anna.week_1;
 
 public class OddOrEven {
+
+/*
+`
+ /*
+    Write  a method which can identifies given number is even or odd
+EX:
+identify(5) ->  "Odd"
+identify(6) ->  "Even"
+
+     */
+
+    public static void oddOrEven(int n){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
