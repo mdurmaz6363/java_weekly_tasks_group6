1c05cbfee6a3412069464eafcf57db1d5591fd3a
Ersin <michael.knight@cydeo.com>
2024-11-21 19:20:56 +0300

    Added Solution of Odd_Or_Even and new question FINRA_Updated

--- src/michael/week_1/Odd_Or_Even.java
+++ src/michael/week_1/Odd_Or_Even.java
@@ -2,22 +2,28 @@ package michael.week_1;
 
 public class Odd_Or_Even {
 
- /*
-    Write  a method which can identify if the given number is even or odd
+    /*
+    Write  a method which can identifies given number is even or odd
 EX:
 identify(5) ->  "Odd"
 identify(6) ->  "Even"
 
      */
 
-
-    public static void oddOrEven(int n) {
-
-
-
-    }
-
     public static void main(String[] args) {
-        oddOrEven(10);
+
+        int num_1 = 5;
+        int num_2 = 6;
+
+        System.out.println("identify Odd Even: " + num_1 + " is " + identifyOddEven(num_1));
+        System.out.println("identify Odd Even: " + num_2 + " is " + identifyOddEven(num_2));
+
     }
+
+    public static String identifyOddEven(int n) {
+        return n % 2 == 0 ? "Even" : "odd";
+    }
+
+
+
 }
