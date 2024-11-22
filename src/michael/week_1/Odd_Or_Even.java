6ea37a57180cc01b9eb60d02373ccab6685383a2
Ersin <michael.knight@cydeo.com>
2024-11-23 00:01:33 +0300

    Updated Odd_Or_Even with 2 solutions

--- src/michael/week_1/Odd_Or_Even.java
+++ src/michael/week_1/Odd_Or_Even.java
@@ -1,5 +1,7 @@
 package michael.week_1;
 
+import java.util.Scanner;
+
 public class Odd_Or_Even {
 
     /*
@@ -12,18 +14,48 @@ identify(6) ->  "Even"
 
     public static void main(String[] args) {
 
-        int num_1 = 5;
-        int num_2 = 6;
 
-        System.out.println("identify Odd Even: " + num_1 + " is " + identifyOddEven(num_1));
-        System.out.println("identify Odd Even: " + num_2 + " is " + identifyOddEven(num_2));
+        int number;
+        String answer;
 
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+
+            System.out.print("Please enter a number: ");
+            number = scanner.nextInt();
+
+            oddOrEven(number);
+
+
+            System.out.println("Do you want to reuse the method (Y/N)");
+
+            answer = scanner.next();
+
+        } while (answer.equalsIgnoreCase("Y"));
+
+
+        System.out.println("We can directly pass the argument without using scanner.");
+        oddOrEven(21);
+
+        System.out.println("2nd solution with a return type...");
+
+
+        String str = returnTypeOddOrEven(50);
+        System.out.println(str);
+
+        scanner.close();
     }
 
-    public static String identifyOddEven(int n) {
-        return n % 2 == 0 ? "Even" : "odd";
+    public static void oddOrEven(int n) {
+        if (n % 2 == 0) {
+            System.out.println("Your number " + n + " is even number");
+        } else {
+            System.out.println("Your number " + n + " is odd number");
+        }
     }
 
-
-
+    public static String returnTypeOddOrEven(int num) {
+        return (num % 2 == 0) ? num + "= Number is even" : num + "= Number is odd";
+    }
 }
