5873e7e3abfb6b376a32ceaf20b77b73ed7cc212
KseniiaaK <thekudrevatukh@gmail.com>
2025-01-06 19:45:25 -0600

    Completed the week 5, 6, 7 tasks

---
+++ src/kseniia/week_7/UniqueElements.java
@@ -0,0 +1,36 @@
+package kseniia.week_7;
+
+
+public class UniqueElements {
+
+    public static void main(String[] args) {
+
+        String str = "AAABBBCCCDEF";
+        System.out.println(uniqueChars(str));
+
+    }
+
+    public static String uniqueChars(String str) {
+
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char charactersOfStr = str.charAt(i);
+            if (str.indexOf(charactersOfStr) == str.lastIndexOf(charactersOfStr)) {
+                result += charactersOfStr;
+            }
+        }
+
+        return result;
+    }
+
+}
+
+
+
+
+/*
+`String -- Find the unique
+Write a return method that can find the unique characters from the String
+Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";
+*/
