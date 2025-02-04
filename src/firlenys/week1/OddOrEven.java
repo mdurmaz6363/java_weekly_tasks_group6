adf689e91f13e03611d630462130a9c6ffad1987
Anwr009 <anwrbara07@gmail.com>
2025-02-04 18:40:41 -0500

    Updated Tasks Please Approve.

--- src/firlenys/week1/OddOrEven.java
+++ src/firlenys/week1/OddOrEven.java
@@ -1,28 +1,33 @@
-package michael.week_1;
-
-public class Odd_Or_Even {
-
- /*
-    Write  a method which can identifies given number is even or odd
-EX:
-identify(5) ->  "Odd"
-identify(6) ->  "Even"
-
-     */
 
 
-    public static void  oddOrEven(int n) {
+public class OddOrEven {
 
+            /*
+               Write  a method which can identifies given number is even or odd
+           EX:
+           identify(5) ->  "Odd"
+           identify(6) ->  "Even"
+
+                */
+
+    // added a comment
+
+    public static void oddOrEven(int n) {
+        if (n % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
 
 
     }
 
     public static void main(String[] args) {
+
         oddOrEven(6);
+
     }
 
 }
 
-   public static String OddorEven(int number) {
 
-}
